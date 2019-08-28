086b2d84bcbfdb4de9ff1b89a0ac60489eedd8e4
Nick Green <nick.green@dmwgoup.co.uk>
2019-08-28 12:32:13 +0100

    Updated script to look in right location of hdfs

--- scripts/whats_on_hdfs.sh
+++ scripts/whats_on_hdfs.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-hdfs dfs -ls /
-hdfs dfs -ls /collections
-hdfs dfs -ls /collections/collection1
-hdfs dfs -ls /collections/collection2
+hdfs dfs -ls /aws/qa
+hdfs dfs -ls /aws/qa/collections
+hdfs dfs -ls /aws/qa/collections/collection1
+hdfs dfs -ls /aws/qa/collections/collection2
 
 
 
